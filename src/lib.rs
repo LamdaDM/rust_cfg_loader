af665153e0f2b449d10a1cec75356f15c0e47166
LamdaDM <ericdmcintosh@outlook.com>
2021-12-20 12:17:27 +1000

    add tests and allow empty lines

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,25 @@
 use std::{fs, collections::BTreeMap};
 
-pub fn load(input: &str) -> VariableMap {
-    let contents = fs::read_to_string(input)
-        .unwrap();
+pub fn load(input: &str) -> Option<VariableMap> {
+    match fs::read_to_string(input) {
+        Ok(contents) => Some(get_variable_map(contents)),
+        Err(_) => None,
+    }
+}
 
+fn get_variable_map(contents: String) -> VariableMap {
     let lines = contents
         .split('\n')
         .collect::<Vec<&str>>();
 
     let mut varmap = BTreeMap::<String, String>::new();
 
+    // Search all lines for a pair, and skip line if it does not have one.
     for line in lines {
-        let (var, value) = line.split_once('=').unwrap();
+        let (var, value) = match line.split_once('=') {
+            Some(pair) => pair,
+            None => continue,
+        };
 
         varmap.insert(String::from(var.trim()), String::from(value.trim()));
     }
@@ -29,5 +37,54 @@ impl VariableMap {
             Some(val) => Some(val),
             None => None,
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::get_variable_map;
+
+
+    #[test]
+    fn complete_variable_map() {
+        let contents = "abc = 123\nijk = lmnop";
+
+        let varmap = get_variable_map(contents.to_string());
+        let first = varmap.get_val("abc").unwrap();
+        let second = varmap.get_val("ijk").unwrap();
+        
+        assert_eq!(first, "123");
+        assert_eq!(second, "lmnop");
+    }
+
+    #[test]
+    fn half_variable_map() {
+        let contents = "abc 123\nijk = lm=nop";
+        
+        let varmap = get_variable_map(contents.to_string());
+        let map_size = varmap.0.len();
+        let first = varmap.get_val("ijk").unwrap();
+
+        assert_eq!(map_size, 1);
+        assert_eq!(first, "lm=nop");
+    }
+
+    #[test]
+    fn empty_variable_map() {
+        let contents = "\n\n\n\n\n\n";
+
+        let varmap = get_variable_map(contents.to_string());
+        let size = varmap.0.len();
+
+        assert_eq!(size, 0);
+    }
+
+    #[test]
+    #[should_panic]
+    fn missing_variables() {
+        let contents = "a = 1";
+        
+        let varmap = get_variable_map(contents.to_string());
+        let _ = varmap.get_val("b").unwrap();
     }
 }
